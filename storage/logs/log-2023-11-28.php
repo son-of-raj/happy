d1c270d2de7f7ab3dcc1370f8e4c34e5f078f487
Akash GR <akashgr64@gmail.com>
2023-12-06 11:41:24 +0530

    changes from here

---
+++ storage/logs/log-2023-11-28.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-11-28 11:03:52 --> 404 Page Not Found: Assets/img
+ERROR - 2023-11-28 11:03:54 --> 404 Page Not Found: Uploads/services
+ERROR - 2023-11-28 11:04:27 --> 404 Page Not Found: Uploads/services
+ERROR - 2023-11-28 11:06:03 --> 404 Page Not Found: Uploads/services
---
+++ storage/logs/log-2023-12-06.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-12-06 11:30:06 --> 404 Page Not Found: Uploads/logo
+ERROR - 2023-12-06 11:30:06 --> 404 Page Not Found: Uploads/logo
+ERROR - 2023-12-06 11:30:06 --> 404 Page Not Found: Uploads/logo
+ERROR - 2023-12-06 11:30:06 --> 404 Page Not Found: Uploads/banners
+ERROR - 2023-12-06 11:30:07 --> 404 Page Not Found: Uploads/logo
+ERROR - 2023-12-06 11:30:11 --> 404 Page Not Found: Uploads/logo
+ERROR - 2023-12-06 11:30:11 --> 404 Page Not Found: Uploads/logo
+ERROR - 2023-12-06 11:30:11 --> 404 Page Not Found: Uploads/logo
+ERROR - 2023-12-06 11:30:11 --> 404 Page Not Found: Uploads/banners
+ERROR - 2023-12-06 11:30:11 --> 404 Page Not Found: Uploads/logo
+ERROR - 2023-12-06 11:30:44 --> 404 Page Not Found: Uploads/banners
+ERROR - 2023-12-06 11:31:43 --> 404 Page Not Found: Uploads/banners
+ERROR - 2023-12-06 11:31:59 --> 404 Page Not Found: Uploads/banners
+ERROR - 2023-12-06 11:33:24 --> 404 Page Not Found: Uploads/banners
+ERROR - 2023-12-06 11:36:13 --> 404 Page Not Found: Assets/img
+ERROR - 2023-12-06 11:38:42 --> 404 Page Not Found: Assets/img
+ERROR - 2023-12-06 11:38:42 --> 404 Page Not Found: Assets/js
+ERROR - 2023-12-06 11:38:46 --> 404 Page Not Found: Assets/js
